fbfae2a5d21802b646460de9df6ffef552a95426
bhagyalekshmi.s <bhagyalekshmi.s@saasvaap.com>
2025-08-04 17:09:06 +0530

    loan-appln and loan-emi test done.

--- test/loan-application.test.js
+++ test/loan-application.test.js
@@ -3,5 +3,23 @@ import { html, fixture, expect } from '@open-wc/testing';
 import '../loan-application.js';
 
 describe('LoanApplication', () => {
-  // Write test cases inside this block
+  it('should render and includes dash-board', () =>
+    fixture(html`<loan-application></loan-application>`).then(el => {
+      const dashboard = el.shadowRoot.querySelector('dash-board');
+
+      expect(el).to.exist;
+      expect(dashboard).to.exist;
+    }));
+
+  it('should default title and counter values', () =>
+    fixture(html`<loan-application></loan-application>`).then(el => {
+      expect(el.title).to.equal('Hey there');
+      expect(el.counter).to.equal(5);
+    }));
+
+  it('should increment counter using increment method', () =>
+    fixture(html`<loan-application></loan-application>`).then(el => {
+      el.__increment();
+      expect(el.counter).to.equal(6);
+    }));
 });
--- test/loan-emi-details.test.js
+++ test/loan-emi-details.test.js
@@ -1,6 +1,52 @@
 import { html, fixture, expect } from '@open-wc/testing';
 import '../src/LoanEMIDetails/LoanEMIDetails.js';
+import sinon from 'sinon';
+import { Router } from '@vaadin/router';
 
 describe('Loan EMI details', () => {
-  // Write test cases inside this block
+  before(() => {
+    localStorage.setItem(
+      'emi',
+      JSON.stringify({
+        interestRate: 8.5,
+        monthlyEMI: 15000,
+        principal: 500000,
+        interest: 50000,
+        totalAmount: 550000,
+      })
+    );
+  });
+
+  it('renders EMI details from localStorage', () =>
+    fixture(html`<loanemi-details></loanemi-details>`).then(el => {
+      const content = el.shadowRoot.textContent;
+
+      expect(content).to.include('8.5');
+      expect(content).to.include('15000');
+      expect(content).to.include('500000');
+      expect(content).to.include('50000');
+      expect(content).to.include('550000');
+    }));
+
+  it('navigates to / details on Cancel button click', () =>
+    fixture(html`<loanemi-details></loanemi-details>`).then(el => {
+      const routerStub = sinon.stub(Router, 'go');
+      const cancelBtn = el.shadowRoot.querySelector('.cancel-btn');
+
+      cancelBtn.click();
+
+      expect(routerStub.calledOnceWith('/details')).to.be.true;
+      routerStub.restore();
+    }));
+
+  it('navigates to /customer on Continue button click', () =>
+    fixture(html`<loanemi-details></loanemi-details>`).then(el => {
+      const routerStub = sinon.stub(Router, 'go');
+      const contBtn = el.shadowRoot.querySelector('.continue-btn');
+
+      contBtn.click();
+
+      expect(routerStub.calledOnceWith('/customer')).to.be.true;
+      routerStub.restore();
+    }));
 });
